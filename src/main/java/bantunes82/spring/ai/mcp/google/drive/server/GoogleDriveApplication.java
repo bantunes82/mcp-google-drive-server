03702cb4129af84aac1d5af69bc0252b012dd875
Bruno Romao <bantunes82@gmail.com>
2025-07-31 08:49:11 +0200

    feat(mcp-google-driver-server)-removing useless code

--- src/main/java/bantunes82/spring/ai/mcp/google/drive/server/GoogleDriveApplication.java
+++ src/main/java/bantunes82/spring/ai/mcp/google/drive/server/GoogleDriveApplication.java
@@ -1,29 +1,12 @@
 package bantunes82.spring.ai.mcp.google.drive.server;
 
-import bantunes82.spring.ai.mcp.google.drive.server.services.GoogleDriveService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class GoogleDriveApplication implements CommandLineRunner {
+public class GoogleDriveApplication {
 
-	private GoogleDriveService googleDriveService;
-
-	public GoogleDriveApplication(GoogleDriveService googleDriveService) {
-		this.googleDriveService = googleDriveService;
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		/*var folderName = "Viagens"; // Folder's name.
-		var fileName = "test.doc"; // File's name.
-		var fileContent = "This is a test file content for Google Drive upload."; // File's content.
-
-		googleDriveService.uploadMicrosoftWordFile(folderName, fileName, fileContent);*/
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoogleDriveApplication.class, args);
